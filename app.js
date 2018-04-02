6558ee0c18d1ec3910cffcbb087cb6272a12f5a0
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-04-02 16:08:12 -0700

    draw board done, fixed blurred board

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-function draw_b() {
+function drawBoard() {
   var pong = document.getElementById("pong");
-  var pong_context = pong.getContext("2d");
-  
+  var ctx = pong.getContext("2d");
+  var width = pong.width;
+  var height = pong.height;
+
+  ctx.fillStyle = '#FFE0B5';
+  ctx.fillRect(0,0,pong.width,pong.height);
+  ctx.fillStyle = "white";
+  ctx.fillRect(10, height/2, 10, 50);
+  ctx.fillRect(width-20, height/2, 10, 50);
+  ctx.fillstyle = 'black';
+  ctx.beginPath();
+  ctx.arc(width/2, height/2, 10, 0*Math.PI, 2*Math.PI);
+  ctx.fillStyle = 'black';
+  ctx.fill();
+  ctx.stroke();
+  console.log("done drawing");
 }
