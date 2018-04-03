ee91abc41b2b9c8ed507d85c1f1ab4347df55eda
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-04-02 17:00:54 -0700

    object oriented refactor

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
-function drawBoard() {
-  var pong = document.getElementById("pong");
-  var ctx = pong.getContext("2d");
-  var width = pong.width;
-  var height = pong.height;
+var pong;
+var ctx;
 
-  ctx.fillStyle = '#FFE0B5';
-  ctx.fillRect(0,0,pong.width,pong.height);
-  ctx.fillStyle = "white";
-  ctx.fillRect(10, height/2, 10, 50);
-  ctx.fillRect(width-20, height/2, 10, 50);
-  ctx.fillstyle = 'black';
+function initialize(){
+  pong = document.getElementById("pong");
+  ctx = pong.getContext("2d");
+}
+
+function Ball(x, y, xv, yv){
+  this.xpos = x;
+  this.ypos = y;
+  this.xvel = xv;
+  this.yvel = yv;
+}
+
+Ball.prototype.render = function(){
   ctx.beginPath();
-  ctx.arc(width/2, height/2, 10, 0*Math.PI, 2*Math.PI);
+  ctx.fillstyle = 'black';
+  ctx.arc(this.xpos, this.ypos, 10, 0*Math.PI, 2*Math.PI);
   ctx.fillStyle = 'black';
   ctx.fill();
-  ctx.stroke();
-  console.log("done drawing");
+}
+
+function Paddle(x, y, v){
+  this.xpos = x;
+  this.ypos = y;
+  this.vel = v;
+}
+
+Paddle.prototype.render = function(){
+  ctx.beginPath();
+  ctx.fillStyle = 'white';
+  ctx.fillRect(this.xpos, this.ypos, 10, 50);
+}
+
+function drawBoard(){
+  ctx.beginPath();
+  ctx.fillStyle = '#FFE0B5';
+  ctx.fillRect(0, 0, pong.width, pong.height);
+}
+
+
+function play() {
+  initialize();
+  drawBoard();
+
+  var player = new Paddle(10, pong.height/2, 0);
+  var computer = new Paddle(pong.width-20, pong.height/2, 0);
+
+  player.render();
+  computer.render();
+
+  var ball = new Ball(pong.width/2, pong.height/2, 0, 0);
+
+  ball.render();
 }
